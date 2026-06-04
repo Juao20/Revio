9d2cf62f0a6f86eeafdb327c2696aba1359f58df
Juao20 <juao.aifa@epitech.eu>
2026-06-04 11:53:09 +0100

    feat: config production, VITE_API_URL

--- revio-frontend/src/api/axios.js
+++ revio-frontend/src/api/axios.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api',
 })
 
-// Ajoute le token automatiquement à chaque requête
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
   if (token) {
